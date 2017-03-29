d304ab9b118a31389a6d104a7f8b132e308e7105
Bobby Acosta <bobby@Macintosh.local>
2017-03-29 14:19:04 -0700

    Pushing current work

--- java/src/main/java/com/on_site/kamayan/collections/LinkedList.java
+++ java/src/main/java/com/on_site/kamayan/collections/LinkedList.java
@@ -34,19 +34,42 @@ public class LinkedList {
     }
 
     public LinkedList prepend(Object value) {
-        throw Kamayan.todo(
-            "The prepend(Object) method should prepend the argument to the",
-            "beginning of this LinkedList and increase the size by 1. The",
-            "return value must be this."
-        );
+        // throw Kamayan.todo(
+        //     "The prepend(Object) method should prepend the argument to the",
+        //     "beginning of this LinkedList and increase the size by 1. The",
+        //     "return value must be this."
+        // );
+
+        head = new Node(value, head);
+        size++;
+
+        // return null;
+        return this;
     }
 
     public LinkedList add(Object value) {
-        throw Kamayan.todo(
-            "The add(Object) method should append the argument to the end of",
-            "this LinkedList and increase the size by 1. The return value must",
-            "be this."
-        );
+        // throw Kamayan.todo(
+        //     "The add(Object) method should append the argument to the end of",
+        //     "this LinkedList and increase the size by 1. The return value must",
+        //     "be this."
+        // );
+
+        Node node = new Node(value);
+
+        head = node;
+
+        if (head == null) {
+            head = node;
+        } else {
+            Node currentNode = head;
+            while (currentNode.child != null){
+                currentNode = currentNode.child;
+            }
+
+            currentNode.child = node;
+        }
+
+        return this;
     }
 
     public Object delete(int index) {
